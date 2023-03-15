d0f14396a0ed9ef390e8d4d8a6c8a9f62e69db73
webpon <2249096563@qq.com>
2023-03-15 12:49:45 +0800

    部署chatgpt

--- chatgpt/registerSW.js
+++ chatgpt/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/chatpgt/sw.js', { scope: '/chatpgt/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
